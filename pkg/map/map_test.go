1f847a2d4a01875beb7bef5e044f64f60760e9b0
yowainwright <yowainwright@gmail.com>
2022-09-16 15:23:17 -0700

    fix: adds another test

--- pkg/map/map_test.go
+++ pkg/map/map_test.go
@@ -10,8 +10,18 @@ func addLetter(str string) string {
 	return str + "a"
 }
 
+func addNumber(num int) int {
+	return num + 1
+}
+
 func TestSliceMapWithString(t *testing.T) {
 	strs := []string{"a", "b", "c"}
 	out := SliceMap(strs, addLetter)
 	assert.Equal(t, []string{"aa", "ba", "ca"}, out, "output should be equal")
+}
+
+func TestSlickMapWithNumber(t *testing.T) {
+	nums := []int{1, 2, 3}
+	out := SliceMap(nums, addNumber)
+	assert.Equal(t, []int{2, 3, 4}, out, "output should be equal")
 }
